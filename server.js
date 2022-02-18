50d0861bc694e98ccb7572077a34cfc020380d09
Kishan Bhatt <iamkishanbhatt@gmail.com>
2022-02-18 11:56:24 +0530

    production changes

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv').config()
 const colors = require('colors')
@@ -16,6 +17,14 @@ app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/goals', require('./routes/goalRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
+
+// Server Client
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html')))
+} else {
+    app.get('/', (req,res) => res.send('Please set to production'))
+}
 
 app.use(errorHandler)
 
